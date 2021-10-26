22381bce13b428f93b6d8ef3febabe98a4498afb
InsaneSuperSand <necronomicon93@gmail.com>
2021-10-26 20:50:38 +0200

    condensed JS

--- js/index.js
+++ js/index.js
@@ -1,73 +1,22 @@
-const suprqMean = 3.93;
-const usabilityMean = 4.06;
-const trustMean = 3.8;
-const loyaltyMean = 3.91;
-const appearanceMean = 3.88;
+const v = {
+  suprqMean: 3.93,
+  usabilityMean: 4.06,
+  trustMean: 3.8,
+  loyaltyMean: 3.91,
+  appearanceMean: 3.88,
+  suprqSD: 0.29,
+  usabilitySD: 0.29,
+  trustSD: 0.52,
+  loyaltySD: 0.46,
+  appearanceSD: 0.25,
+};
 
-const suprqSD = 0.29;
-const usabilitySD = 0.29;
-const trustSD = 0.52;
-const loyaltySD = 0.46;
-const appearanceSD = 0.25;
-
-
-
-let math = 
-
-
-/*
-{
-  const input = document.querySelector("#formGroupExampleInput");
-  const log = document.getElementById("#suprq");
-
-  input.addEventListener("change", updateValue);
-
-  function updateValue(e) {
-    suprq.textContent = (e.target.value - suprqMean) / suprqSD;
+const tt = ["suprq", "usability", "trust", "loyalty", "appearance"];
+tt.map((t) => {
+  {
+    document.querySelector(`#${t}Input`).addEventListener("change", (e) => {
+      document.getElementById(`${t}`).textContent =
+        (e.target.value - v[`${t}Mean`]) / v[`${t}SD`];
+    });
   }
-}
-
-{
-  const input = document.querySelector("#formGroupExampleInput1");
-  const log = document.getElementById("#usability");
-
-  input.addEventListener("change", updateValue);
-
-  function updateValue(e) {
-    usability.textContent = (e.target.value - usabilityMean) / usabilitySD;
-  }
-}
-
-{
-  const input = document.querySelector("#formGroupExampleInput2");
-  const log = document.getElementById("#trust");
-
-  input.addEventListener("change", updateValue);
-
-  function updateValue(e) {
-    trust.textContent = (e.target.value - trustMean) / trustSD;
-  }
-}
-
-{
-  const input = document.querySelector("#formGroupExampleInput3");
-  const log = document.getElementById("#loyalty");
-
-  input.addEventListener("change", updateValue);
-
-  function updateValue(e) {
-    loyalty.textContent = (e.target.value - loyaltyMean) / loyaltySD;
-  }
-}
-
-{
-  const input = document.querySelector("#formGroupExampleInput4");
-  const log = document.getElementById("#appearance");
-
-  input.addEventListener("change", updateValue);
-
-  function updateValue(e) {
-    appearance.textContent = (e.target.value - appearanceMean) / appearanceSD;
-  }
-}
-*/
+});
